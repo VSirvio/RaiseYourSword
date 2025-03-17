f134ebaecd735685f7638320c4a1657f2096ff66
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-03-17 16:08:52 +0200

    Create Maksukortti object for tests in SetUp()

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -3,24 +3,20 @@ from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
-        print("Set up goes here")
+        self.kortti = Maksukortti(1000)
 
     def test_konstruktori_asettaa_saldon_oikein(self):
-        kortti = Maksukortti(1000)
-
-        self.assertEqual(kortti.saldo_euroina(), 10.0)
+        self.assertEqual(self.kortti.saldo_euroina(), 10.0)
 
     def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
-        kortti = Maksukortti(1000)
-        kortti.syo_edullisesti()
+        self.kortti.syo_edullisesti()
 
-        self.assertEqual(kortti.saldo_euroina(), 7.5)
+        self.assertEqual(self.kortti.saldo_euroina(), 7.5)
 
     def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
-        kortti = Maksukortti(1000)
-        kortti.syo_maukkaasti()
+        self.kortti.syo_maukkaasti()
 
-        self.assertEqual(kortti.saldo_euroina(), 6.0)
+        self.assertEqual(self.kortti.saldo_euroina(), 6.0)
 
     def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
         kortti = Maksukortti(200)
