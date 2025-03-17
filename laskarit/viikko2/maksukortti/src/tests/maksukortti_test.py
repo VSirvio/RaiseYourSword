f0e6e21e45f6d854766d884f42253617f18fc5f5
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-03-17 16:05:47 +0200

    Create first 4 tests for viikko2/maksukortti

---
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -0,0 +1,29 @@
+import unittest
+from maksukortti import Maksukortti
+
+class TestMaksukortti(unittest.TestCase):
+    def setUp(self):
+        print("Set up goes here")
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        kortti = Maksukortti(1000)
+
+        self.assertEqual(kortti.saldo_euroina(), 10.0)
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        kortti = Maksukortti(1000)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(kortti.saldo_euroina(), 7.5)
+
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+        kortti = Maksukortti(1000)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 6.0)
+
+    def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(200)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(kortti.saldo_euroina(), 2.0)
