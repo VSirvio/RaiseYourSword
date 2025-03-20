6c8bf55f153ae70c9cf8743e38efbe5344b7499b
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-03-20 15:21:26 +0200

    Implement simple pygame application

--- src/index.py
+++ src/index.py
@@ -1,5 +1,25 @@
+import pygame
+
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGHT = 600
+
 def main():
-    print("Hello world!")
+    display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
+    pygame.display.set_caption("Raise Your Sword")
+
+    pygame.init()
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.update()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
