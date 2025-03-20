a91e259809f18f3b24cdcdb7ef7c1a440358db40
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-03-20 15:24:43 +0200

    Draw a single background tile on the screen

--- src/index.py
+++ src/index.py
@@ -1,12 +1,18 @@
+import os
+
 import pygame
 
 DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 600
 
+dirname = os.path.dirname(__file__)
+
 def main():
     display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 
     pygame.display.set_caption("Raise Your Sword")
+
+    bg = pygame.image.load(os.path.join(dirname, "assets", "background_tile_grass.png")).convert()
 
     pygame.init()
 
@@ -16,6 +22,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        display.blit(bg, (0, 0))
 
         pygame.display.update()
 
