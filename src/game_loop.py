3d7564996f116b1b90b35819a0b32d4e6d845861
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-03-25 10:06:08 +0200

    Modify GameLoop.__handle_events to be more concise

--- src/game_loop.py
+++ src/game_loop.py
@@ -27,45 +27,34 @@ class GameLoop:
 
     def __handle_events(self):
         for event in self.__event_queue.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key in (pygame.K_DOWN, pygame.K_s):
-                    self.__key_pressed["down"] = True
-                elif event.key in (pygame.K_UP, pygame.K_w):
-                    self.__key_pressed["up"] = True
-                elif event.key in (pygame.K_LEFT, pygame.K_a):
-                    self.__key_pressed["left"] = True
-                elif event.key in (pygame.K_RIGHT, pygame.K_d):
-                    self.__key_pressed["right"] = True
-                elif event.key in (pygame.K_RSHIFT, pygame.K_LSHIFT):
-                    self.__game.attack()
-            elif event.type == pygame.KEYUP:
-                if event.key in (pygame.K_DOWN, pygame.K_s):
-                    self.__key_pressed["down"] = False
-                elif event.key in (pygame.K_UP, pygame.K_w):
-                    self.__key_pressed["up"] = False
-                elif event.key in (pygame.K_LEFT, pygame.K_a):
-                    self.__key_pressed["left"] = False
-                elif event.key in (pygame.K_RIGHT, pygame.K_d):
-                    self.__key_pressed["right"] = False
-            elif event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 return False
 
+            if event.type not in (pygame.KEYDOWN, pygame.KEYUP):
+                continue
+
+            match event.key:
+                case pygame.K_DOWN | pygame.K_s:
+                    self.__key_pressed["down"] = event.type == pygame.KEYDOWN
+                case pygame.K_UP | pygame.K_w:
+                    self.__key_pressed["up"] = event.type == pygame.KEYDOWN
+                case pygame.K_LEFT | pygame.K_a:
+                    self.__key_pressed["left"] = event.type == pygame.KEYDOWN
+                case pygame.K_RIGHT | pygame.K_d:
+                    self.__key_pressed["right"] = event.type == pygame.KEYDOWN
+                case pygame.K_RSHIFT | pygame.K_LSHIFT if event.type == pygame.KEYDOWN:
+                    self.__game.attack()
+
         self.__vert_direction = None
-        if self.__key_pressed["up"]:
-            if self.__key_pressed["down"]:
-                self.__vert_direction = None
-            else:
-                self.__vert_direction = "up"
-        elif self.__key_pressed["down"]:
+        if self.__key_pressed["up"] and not self.__key_pressed["down"]:
+            self.__vert_direction = "up"
+        elif self.__key_pressed["down"] and not self.__key_pressed["up"]:
             self.__vert_direction = "down"
 
         self.__horiz_direction = None
-        if self.__key_pressed["left"]:
-            if self.__key_pressed["right"]:
-                self.__horiz_direction = None
-            else:
-                self.__horiz_direction = "left"
-        elif self.__key_pressed["right"]:
+        if self.__key_pressed["left"] and not self.__key_pressed["right"]:
+            self.__horiz_direction = "left"
+        elif self.__key_pressed["right"] and not self.__key_pressed["left"]:
             self.__horiz_direction = "right"
 
         return True
