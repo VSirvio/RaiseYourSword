17f9d8db41e13e63c5e8e6b6eadf3a50d04ad56c
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-04-24 10:32:50 +0300

    Commit 'events.py' that was forgotten earlier

---
+++ src/events.py
@@ -0,0 +1,12 @@
+class MovementDirectionChanged:
+    def __init__(self, new_direction):
+        self.new_direction = new_direction
+
+class AttackStarted:
+    pass
+
+class AnimationFinished:
+    pass
+
+class Lose:
+    pass
