abdd58abe61d74d674f2293878d85a67b129df2a
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-04-26 16:45:32 +0300

    Document Renderer class and its methods

--- src/renderer.py
+++ src/renderer.py
@@ -1,7 +1,17 @@
 import pygame
 
 class Renderer:
+    """Provides the game means to draw its graphics on the screen."""
+
     def __init__(self, display, game, graphics_scaling_factor):
+        """Creates a new renderer with the given parameters.
+
+        Args:
+            display: Display surface returned by pygame.display.set_mode().
+            game: Game object whose graphics are to be rendered.
+            graphics_scaling_factor: Size of one game pixel in screen pixels.
+        """
+
         self.__display = display
         self.__game = game
         self.__graphics_scaling_factor = graphics_scaling_factor
@@ -12,6 +22,8 @@ class Renderer:
         ))
 
     def render(self):
+        """Renders game graphics scaled by the factor on the display surface."""
+
         self.__game.draw(self.__intermediate_surface)
 
         scaled_surface = pygame.transform.scale_by(
