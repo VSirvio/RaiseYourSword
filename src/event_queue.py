246fac79d040fe9808b49119d1d9089e5cb70e7c
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-04-28 16:11:06 +0300

    Document EventQueue class and its methods

--- src/event_queue.py
+++ src/event_queue.py
@@ -1,5 +1,13 @@
 import pygame
 
 class EventQueue:
+    """Provides a way to get keyboard and mouse input from the user."""
+
     def get(self):
+        """Removes and returns the next input event from the queue.
+
+        Returns:
+            The next input event in the queue as a "pygame.event.Event" object.
+        """
+
         return pygame.event.get()
