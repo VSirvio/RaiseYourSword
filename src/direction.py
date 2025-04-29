e59b3e31dad506067e3c2e53787c122bc010ef82
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-04-29 13:04:25 +0300

    Document Direction class and its methods

--- src/direction.py
+++ src/direction.py
@@ -11,21 +11,51 @@ class HorizontalDirection(Enum):
     RIGHT = "RIGHT"
 
 class Direction:
+    """2D direction with 8 possible direction values and a "none" value."""
+
     def __init__(self, vertical_component, horizontal_component):
+        """Creates a new direction object.
+
+        Args:
+            vertical_component: A direction.VerticalDirection instance.
+            horizontal_component: A direction.HorizontalDirection instance.
+        """
+
         self.__vertical_component = vertical_component
         self.__horizontal_component = horizontal_component
 
     def __eq__(self, other):
+        """Two Directions are equal if and only if their components are equal.
+
+        Args:
+            other: A Direction instance.
+
+        Returns:
+            Boolean indicating whether this direction is equal to the other.
+        """
+
         return (self.vertical_component == other.vertical_component and
             self.horizontal_component == other.horizontal_component)
 
     def __ne__(self, other):
+        """Two Directions are not equal if and only if __eq__() returns False.
+
+        Args:
+            other: A Direction instance.
+
+        Returns:
+            Boolean indicating whether this direction is not equal to the other.
+        """
         return not self == other
 
     def __hash__(self):
+        """The hash of the tuple (vertical_component, horizontal_component)."""
+
         return hash((self.__vertical_component, self.__horizontal_component))
 
     def __repr__(self):
+        """String of the form "<vertical_component,horizontal_component>"."""
+
         if self.__vertical_component == VerticalDirection.NONE:
             return f"<{self.__horizontal_component}>"
         if self.__horizontal_component == HorizontalDirection.NONE:
@@ -34,18 +64,26 @@ class Direction:
         return f"<{self.__vertical_component},{self.__horizontal_component}>"
 
     def __str__(self):
+        """Returns the same value as __repr__()."""
+
         return repr(self)
 
     @property
     def vertical_component(self):
+        """The direction.VerticalDirection instance of this Direction."""
+
         return self.__vertical_component
 
     @property
     def horizontal_component(self):
+        """The direction.HorizontalDirection instance of this Direction."""
+
         return self.__horizontal_component
 
     @property
     def movement_vector(self):
+        """The tuple (dx, dy), where dx and dy can have values -1, 0 or 1."""
+
         dx = 0
         dy = 0
 
@@ -62,6 +100,8 @@ class Direction:
         return dx, dy
 
     def clip_to_four_directions(self):
+        """Returns horizontal component or if it is none, vertical component."""
+
         if self.__horizontal_component == HorizontalDirection.LEFT:
             return LEFT
         if self.__horizontal_component == HorizontalDirection.RIGHT:
