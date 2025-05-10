a5348e681a0db5851d7aa2c7c04fbe47704a74fb
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-05-10 20:11:29 +0300

    Document src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,25 @@
 class Range:
+    """Stores the minimum and maximum values of a range."""
+
     def __init__(self, minimum, maximum):
+        """Create a range with the given minimum and maximum values.
+
+        Args:
+            minimum: The smallest integer value included in the range.
+            maximum: The largest integer value included in the range.
+        """
+
         self.__minimum = minimum
         self.__maximum = maximum
 
     @property
     def minimum(self):
+        """The smallest integer value included in this range."""
+
         return self.__minimum
 
     @property
     def maximum(self):
+        """The largest integer value included in this range."""
+
         return self.__maximum
